011d6feb63b667419e4ad6712be3b1a149df9830
oldentomato <ender1412@naver.com>
2023-02-26 19:50:23 +0900

    기타 버그 수정5

--- feature_store_docker/example_repo.py
+++ feature_store_docker/example_repo.py
@@ -7,7 +7,7 @@ from feast.types import String
 msg_id = Entity(name="id", value_type=ValueType.INT64)
 
 msg_hourly_datas = FileSource(
-    path="/workspace/code/datas/msg_data.parquet",
+    path="./msg_data.parquet",
     event_timestamp_column="event_timestamp"
 )
 
